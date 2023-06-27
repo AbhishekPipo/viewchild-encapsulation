7093b617fd7ae96d10bd552abf90e81577b813d8
AbhishekPipo <Pipo.abhi@gmail.com>
2023-06-27 17:23:04 +0530

    first-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { CompComponent } from './comp/comp.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    CompComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
